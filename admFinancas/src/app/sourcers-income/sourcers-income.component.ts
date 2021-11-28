75698f7701414f08fe19ac211a26e1f21727a82a
gnl2 <gnl2@cin.ufpe.br>
2021-11-28 16:52:54 -0300

    feat(sources-income): cria o componente e formulário html

---
+++ admFinancas/src/app/sourcers-income/sourcers-income.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sourcers-income',
+  templateUrl: './sourcers-income.component.html',
+  styleUrls: ['./sourcers-income.component.css']
+})
+export class SourcersIncomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
