22716350d9c123ec85ccc6a5dcd78dd399214902
gnl2 <gnl2@cin.ufpe.br>
2021-11-28 16:50:25 -0300

    feat(graphical-reports): cria o componente

---
+++ admFinancas/src/app/graphical-reports/graphical-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graphical-reports',
+  templateUrl: './graphical-reports.component.html',
+  styleUrls: ['./graphical-reports.component.css']
+})
+export class GraphicalReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
