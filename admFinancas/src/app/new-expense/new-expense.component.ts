cf0325f36a135dcbfb6b84624b66c2346ca7b70d
gnl2 <gnl2@cin.ufpe.br>
2021-11-28 16:50:45 -0300

    feat(new-expense): cria o componente

---
+++ admFinancas/src/app/new-expense/new-expense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-expense',
+  templateUrl: './new-expense.component.html',
+  styleUrls: ['./new-expense.component.css']
+})
+export class NewExpenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
