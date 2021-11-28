df91017a04f7cdc076cb1e1448572cf4ee54e256
gnl2 <gnl2@cin.ufpe.br>
2021-11-28 16:53:53 -0300

    chore(app.module): adiciona módulos dos novos componentes e componentes do material

--- admFinancas/src/app/app.module.ts
+++ admFinancas/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SourcersIncomeComponent } from './sourcers-income/sourcers-income.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { NewExpenseComponent } from './new-expense/new-expense.component';
+import { GraphicalReportsComponent } from './graphical-reports/graphical-reports.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+// Material design
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SourcersIncomeComponent,
+    ExpensesComponent,
+    NewExpenseComponent,
+    GraphicalReportsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
