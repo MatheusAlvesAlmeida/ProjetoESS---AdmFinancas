07dec3f8ee2702f1b613030c13fc1cbb0e02b9bf
gnl2 <gnl2@cin.ufpe.br>
2021-11-28 16:53:23 -0300

    chore(app-routing): cria rotas para os novos componentes

--- admFinancas/src/app/app-routing.module.ts
+++ admFinancas/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { GraphicalReportsComponent } from './graphical-reports/graphical-reports.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewExpenseComponent } from './new-expense/new-expense.component';
+import { SourcersIncomeComponent } from './sourcers-income/sourcers-income.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'sourcesIncome', component: SourcersIncomeComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: 'graphicalReports', component: GraphicalReportsComponent },
+  { path: 'newExpense', component: NewExpenseComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
