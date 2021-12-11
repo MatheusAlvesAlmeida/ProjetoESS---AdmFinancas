c5ca1025070d767d5ada0ab1b86604cff538c29f
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2021-12-11 14:21:18 -0300

    add: edit dialog to make expenses editable

--- admFinancas/src/app/expenses/components/expenses-form/expenses-form.component.ts
+++ admFinancas/src/app/expenses/components/expenses-form/expenses-form.component.ts
@@ -4,11 +4,12 @@ import { MatTable } from '@angular/material/table';
 import {MatDialog} from '@angular/material/dialog';
 import { ExpensesTable } from '../../types/expenses';
 import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
+import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 
 const ELEMENT_DATA: ExpensesTable[] = [
-  {id: 1, percentage: 20, type: 'Alimentação'},
-  {id: 2, percentage: 30, type: 'Aluguel'},
-  {id: 3, percentage: 25, type: 'Treino'}
+  {id: 0, percentage: 20, type: 'Alimentação'},
+  {id: 1, percentage: 30, type: 'Aluguel'},
+  {id: 2, percentage: 25, type: 'Treino'}
 ];
 
 @Component({
@@ -37,7 +38,7 @@ export class ExpensesFormComponent implements OnInit {
       this.inputError();
       return;
     }
-    const newElement = {id: 1, percentage: this.enterPercentage, type: this.enterType}
+    const newElement = {id: this.dataSource.length, percentage: this.enterPercentage, type: this.enterType}
     this.dataSource.push(newElement);
     if(this.table) this.table.renderRows();
   }
@@ -47,8 +48,23 @@ export class ExpensesFormComponent implements OnInit {
     if(this.table) this.table.renderRows();
   }
 
-  editData(){
-    
+  editData(id: number){
+    const dialogRef = this.dialog.open(EditDialogComponent, {
+      width: '250px',
+      data: {id: id, percentage: this.dataSource[id].percentage, type: this.dataSource[id].type},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.enterPercentage = result.percentage;
+      this.enterType = result.type;
+      if(this.checkInput()){
+        this.inputError();
+        return;
+      }
+      this.dataSource[id] = result;
+      if(this.table) this.table.renderRows();
+    });
   }
 
   checkInput(){
