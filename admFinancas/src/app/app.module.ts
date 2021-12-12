f582bff943b12337e3c246520237330dcd8913b2
gnl2 <gnl2@cin.ufpe.br>
2021-12-12 16:02:48 -0300

    chore: mapeia os novos módulos ao app module

--- admFinancas/src/app/app.module.ts
+++ admFinancas/src/app/app.module.ts
@@ -5,54 +5,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { SourcersIncomeComponent } from './sourcers-income/sourcers-income.component';
-import { NewExpenseComponent } from './new-expense/new-expense.component';
-import { GraphicalReportsComponent } from './graphical-reports/graphical-reports.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { LoginModule } from './login/login.module';
+import { HomeModule } from './home/home.module';
+import { SourcesIncomeModule } from './sources-income/sources-income.module';
+import { SharedModule } from './shared/shared.module';
 import { ExpensesModule } from './expenses/expenses.module';
 
-
 // Material design
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { FirebaseService } from './services/firebase.service';
 import { AngularFireModule } from '@angular/fire/compat';
-import { SigninComponent } from './signin/signin.component';
+import { SignInModule } from './signin/signin.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    SourcersIncomeComponent,
-    NewExpenseComponent,
-    GraphicalReportsComponent,
-    NavbarComponent,
-    SigninComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
     MatSelectModule,
     MatIconModule,
     FormsModule,
     ExpensesModule,
+    SourcesIncomeModule,
+    SignInModule,
+    SharedModule,
+    LoginModule,
+    HomeModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
