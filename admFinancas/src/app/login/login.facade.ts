132d586ea8c96eb43c4b2c282f549a686f0996e3
gnl2 <gnl2@cin.ufpe.br>
2021-12-12 15:55:08 -0300

    refact(login): cria módulo de login

---
+++ admFinancas/src/app/login/login.facade.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+import { LoginApi } from './api/login.api';
+import { LoginState } from './state/login.state';
+
+@Injectable()
+export class LoginFacade {}
--- admFinancas/src/app/login/pages/login.component.ts
+++ admFinancas/src/app/login/pages/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { FirebaseService } from '../services/firebase.service';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-login',
