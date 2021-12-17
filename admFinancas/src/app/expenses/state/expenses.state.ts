1e715b6bf85564ac6393430ac26d750599c80d57
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2021-12-17 17:57:32 -0300

    add: expenses state file

--- admFinancas/src/app/expenses/state/expenses.state.ts
+++ admFinancas/src/app/expenses/state/expenses.state.ts
@@ -1,5 +1,18 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ExpensesTable } from '../../../../../common/expenses';
 
 @Injectable({ providedIn: 'root' })
-export class ExpensesState {}
+export class ExpensesState {
+  private expensesTable: BehaviorSubject<ExpensesTable[]> = new BehaviorSubject(
+    [] as ExpensesTable[]
+  );
+
+  public getExpensesTable() {
+    return this.expensesTable.asObservable();
+  }
+
+  public setExpensesTable(newExpenses: ExpensesTable[]) {
+    this.expensesTable.next(newExpenses);
+  }
+}
