d74452981d063f036af4c43dbecd739fff9e3b53
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2021-12-17 09:57:52 -0300

    func: exporting server onport 3000

--- backend/api.ts
+++ backend/api.ts
@@ -1,7 +1,28 @@
-import express = require('express');
+import express = require("express");
 import bodyParser = require("body-parser");
-import nodemailer = require("nodemailer")
+import nodemailer = require("nodemailer");
 const cron = require("node-cron");
 
-import { Expenses } from '../common/expenses';
+import { Expenses } from "../common/expenses";
 
+var api = express();
+
+var allowCrossDomain = function (req: any, res: any, next: any) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+api.use(allowCrossDomain);
+
+api.use(bodyParser.json());
+
+var server = api.listen(3000, function () {
+  console.log("Listening on port 3000!");
+});
+
+function closeServer(): void {
+  server.close();
+}
+
+export { server, closeServer };
