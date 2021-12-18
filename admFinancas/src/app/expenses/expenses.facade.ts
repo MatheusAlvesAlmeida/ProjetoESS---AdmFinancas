259360b3eb01eb640026c912de98a3a9274c2ac9
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2021-12-18 09:34:08 -0300

    add: facade and state for expenses

--- admFinancas/src/app/expenses/expenses.facade.ts
+++ admFinancas/src/app/expenses/expenses.facade.ts
@@ -1,6 +1,32 @@
 import { Injectable } from '@angular/core';
+import { ExpensesTable } from '../../../../common/expenses';
 import { ExpensesApi } from './api/expenses.api';
 import { ExpensesState } from './state/expenses.state';
 
 @Injectable()
-export class ExpensesFacade {}
+export class ExpensesFacade {
+  private originalExpensesTable: ExpensesTable[] = [];
+
+  public constructor(
+    private readonly state: ExpensesState,
+    private readonly api: ExpensesApi
+  ) {}
+
+  public getExpensesTable() {
+    return this.state.getExpensesTable();
+  }
+
+  public fetchExpensesTable() {
+    this.state.setLoading(true);
+    this.api.getExpensesTable().subscribe({
+      next: (data) => {
+        this.state.setExpensesTable(data);
+      },
+      complete: () => this.state.setLoading(false),
+    });
+  }
+
+  public isLoading() {
+    return this.state.isLoading();
+  }
+}
--- admFinancas/src/app/expenses/state/expenses.state.ts
+++ admFinancas/src/app/expenses/state/expenses.state.ts
@@ -4,9 +4,20 @@ import { ExpensesTable } from '../../../../../common/expenses';
 
 @Injectable({ providedIn: 'root' })
 export class ExpensesState {
+  private loading: BehaviorSubject<boolean> = new BehaviorSubject(
+    false as boolean
+  );
   private expensesTable: BehaviorSubject<ExpensesTable[]> = new BehaviorSubject(
     [] as ExpensesTable[]
   );
+
+  public isLoading() {
+    return this.loading.asObservable();
+  }
+
+  public setLoading(loading: boolean) {
+    this.loading.next(loading);
+  }
 
   public getExpensesTable() {
     return this.expensesTable.asObservable();
