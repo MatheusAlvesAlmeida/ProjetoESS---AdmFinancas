3fbc966eace032dbef77dcb58495795a005a4d8e
gnl2 <gnl2@cin.ufpe.br>
2021-12-20 01:46:09 -0300

    feat(sources-income): adiciona métodos ao state

--- admFinancas/src/app/sources-income/state/sources-income.state.ts
+++ admFinancas/src/app/sources-income/state/sources-income.state.ts
@@ -1,5 +1,29 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { SourcesIncomeTable } from '../../../../../common/sources-income';
 
 @Injectable({ providedIn: 'root' })
-export class SourcesIncomeState {}
+export class SourcesIncomeState {
+    private loading: BehaviorSubject<boolean> = new BehaviorSubject(
+        false as boolean
+      );
+      private expensesTable: BehaviorSubject<SourcesIncomeTable[]> = new BehaviorSubject(
+        [] as SourcesIncomeTable[]
+      );
+    
+      public isLoading() {
+        return this.loading.asObservable();
+      }
+    
+      public setLoading(loading: boolean) {
+        this.loading.next(loading);
+      }
+    
+      public getSourcesIncomeTable() {
+        return this.expensesTable.asObservable();
+      }
+    
+      public setSourcesIncomeTable(newExpenses: SourcesIncomeTable[]) {
+        this.expensesTable.next(newExpenses);
+      }
+}
