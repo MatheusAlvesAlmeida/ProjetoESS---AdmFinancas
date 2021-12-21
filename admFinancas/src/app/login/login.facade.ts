1b13998928403f1dfa4101359b59318262aab561
gnl2 <gnl2@cin.ufpe.br>
2021-12-20 22:45:27 -0300

    feat(login): completa ciclo do login do usuário

--- admFinancas/src/app/login/login.facade.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@angular/core';
-import { LoginApi } from './api/login.api';
-import { LoginState } from './state/login.state';
-
-@Injectable()
-export class LoginFacade {}
